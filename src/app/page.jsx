3acd89ed27d3f29932397de90c8f6b6453a929fe
Benito Tridella Dolce <benitotridelladolce@gmail.com>
2025-07-24 10:40:14 -0300

    Add. select component to choose language.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,13 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input";
 import { ModeToggle } from "@/components/ModeToggle";
 import { ToggleSwitch } from "@/components/ui/toggle-switch";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 export default function Home() {
   return (
@@ -11,9 +18,18 @@ export default function Home() {
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-regex"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M6.5 15a2.5 2.5 0 1 1 0 5a2.5 2.5 0 0 1 0 -5z" /><path d="M17 7.875l3 -1.687" /><path d="M17 7.875v3.375" /><path d="M17 7.875l-3 -1.687" /><path d="M17 7.875l3 1.688" /><path d="M17 4.5v3.375" /><path d="M17 7.875l-3 1.688" /></svg>
           <span className="font-semibold">Know Your Regex</span>
         </div>
-        <hr className="h-6 border-r-1"/>
+        <hr className="h-6 border-r-1" />
         <ModeToggle />
         <span>v0.0.1</span>
+        <Select>
+          <SelectTrigger className="w-auto">
+            <SelectValue placeholder="ES" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="ES">ES</SelectItem>
+            <SelectItem value="EN">EN</SelectItem>
+          </SelectContent>
+        </Select>
       </div>
       <div className="flex flex-col relative items-center h-screen justify-end mb-12">
         <ToggleSwitch />
