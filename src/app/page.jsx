a66ec6402871791a1684b70361ffe7d95dc72590
Benito Tridella Dolce <benitotridelladolce@gmail.com>
2025-08-05 11:44:19 -0300

    Starting with componentization

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,8 @@
 'use client';
 import { useChat } from '@ai-sdk/react';
 import { useQueryState } from 'nuqs'
-import { ModeToggle } from "@/components/ModeToggle";
 import { AIChatInput } from "@/components/ui/ai-chat-input"
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
+import { Navbar } from '@/components/ui/navbar';
 
 export default function Home() {
   const [activeOption] = useQueryState("type");
@@ -25,24 +18,7 @@ export default function Home() {
 
   return (
     <div className="flex flex-col h-screen my-auto items-center">
-      <div className="flex gap-3 items-center mt-6 sm:gap-5">
-        <div className="flex gap-2 items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-regex"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M6.5 15a2.5 2.5 0 1 1 0 5a2.5 2.5 0 0 1 0 -5z" /><path d="M17 7.875l3 -1.687" /><path d="M17 7.875v3.375" /><path d="M17 7.875l-3 -1.687" /><path d="M17 7.875l3 1.688" /><path d="M17 4.5v3.375" /><path d="M17 7.875l-3 1.688" /></svg>
-          <span className="font-semibold">Know Your Regex</span>
-        </div>
-        <hr className="h-6 border-r-3" />
-        <ModeToggle />
-        <span>v0.0.1</span>
-        <Select>
-          <SelectTrigger className="w-auto">
-            <SelectValue placeholder="ES" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="ES">ES</SelectItem>
-            <SelectItem value="EN">EN</SelectItem>
-          </SelectContent>
-        </Select>
-      </div>
+      <Navbar />
 
       <div className="flex flex-col w-full max-w-md py-10 mx-auto stretch">
         <div className='h-100 overflow-y-scroll px-6 sm:px-0'>
--- src/components/ui/navbar.jsx
+++ src/components/ui/navbar.jsx
@@ -1,6 +1,26 @@
+import { ModeToggle } from "../ModeToggle"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select"
 function Navbar() {
     return (
-        <div>navbar</div>
+        <div className="flex gap-3 items-center mt-6 sm:gap-5">
+            <div className="flex gap-2 items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-regex"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M6.5 15a2.5 2.5 0 1 1 0 5a2.5 2.5 0 0 1 0 -5z" /><path d="M17 7.875l3 -1.687" /><path d="M17 7.875v3.375" /><path d="M17 7.875l-3 -1.687" /><path d="M17 7.875l3 1.688" /><path d="M17 4.5v3.375" /><path d="M17 7.875l-3 1.688" /></svg>
+                <span className="font-semibold">Know Your Regex</span>
+            </div>
+            <hr className="h-6 border-r-3" />
+            <ModeToggle />
+            <Select>
+                <SelectTrigger className="w-auto">
+                    <SelectValue placeholder="ES" />
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectItem value="ES">ES</SelectItem>
+                    <SelectItem value="EN">EN</SelectItem>
+                </SelectContent>
+            </Select>
+            <hr className="h-6 border-r-3" />
+            <span>v0.0.1</span>
+        </div>
     )
 }
 export { Navbar }
