c2ace150de254fd2c25423d9348298724d63b474
Benito Tridella Dolce <benitotridelladolce@gmail.com>
2025-09-02 10:25:13 -0300

    Add. Content to the sidebar

--- src/components/app-sidebar.jsx
+++ src/components/app-sidebar.jsx
@@ -1,15 +1,33 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
-
+'use client'
+import Link from 'next/link'
+import { Calendar, Home, Inbox, Search, Settings, Plus, Linkedin, Github, Globe } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
 import {
   Sidebar,
   SidebarContent,
   SidebarGroup,
-  SidebarGroupContent,
+  SidebarFooter,
   SidebarGroupLabel,
   SidebarMenu,
+  SidebarHeader,
   SidebarMenuButton,
+  SidebarGroupAction,
+  SidebarGroupContent,
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
+
+import {
+  Dialog,
+  DialogContent,
+  DialogClose,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter
+} from "@/components/ui/dialog"
 
 // Menu items.
 const items = [
@@ -43,25 +61,104 @@ const items = [
 export function AppSidebar({ ...props }) {
   return (
     <Sidebar {...props}>
+      <SidebarHeader>
+        <div className="flex gap-2 mx-auto p-2">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-regex"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M6.5 15a2.5 2.5 0 1 1 0 5a2.5 2.5 0 0 1 0 -5z" /><path d="M17 7.875l3 -1.687" /><path d="M17 7.875v3.375" /><path d="M17 7.875l-3 -1.687" /><path d="M17 7.875l3 1.688" /><path d="M17 4.5v3.375" /><path d="M17 7.875l-3 1.688" /></svg>
+          <span className="font-semibold">Know Your Regex</span>
+        </div>
+      </SidebarHeader>
+      <hr className="w-11/12 mx-auto" />
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
+          <SidebarGroupLabel >Tus regex</SidebarGroupLabel>
+          <SidebarGroupAction title="Add Project">
+            <Dialog>
+              <DialogTrigger asChild className="hover:cursor-pointer">
+                <Plus />
+              </DialogTrigger>
+              <form>
+                <DialogContent className="sm:max-w-[425px]">
+                  <DialogHeader>
+                    <DialogTitle>Guarda tu Regex</DialogTitle>
+                    <DialogDescription>
+                      Guarda aqui tu regex con su titulo para poder usarla en cualquier momento
+                    </DialogDescription>
+                  </DialogHeader>
+                  <div className="grid gap-4">
+                    <div className="grid gap-3">
+                      <Label htmlFor="title-1">Titulo</Label>
+                      <Input id="title-1" name="title" placeholder="Titulo de tu regex" />
+                    </div>
+                    <div className="grid gap-3">
+                      <Label htmlFor="regex-1">Regex</Label>
+                      <Input id="regex-1" name="regex" placeholder="^\d{2}-\d{2}-\d{4}$" />
+                    </div>
+                  </div>
+                  <DialogFooter>
+                    <DialogClose asChild>
+                      <Button variant="outline">Cancelar</Button>
+                    </DialogClose>
+                    <Button type="submit">Guardar</Button>
+                  </DialogFooter>
+                </DialogContent>
+              </form>
+            </Dialog>
+          </SidebarGroupAction>
         </SidebarGroup>
+        {/* <SidebarGroup>
+          <div className="flex align-center justify-center hover:cursor-pointer">
+            <Dialog>
+              <form>
+                <DialogTrigger asChild className="hover:cursor-pointer">
+                  <Button variant="outline"><Plus /></Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                  <DialogHeader>
+                    <DialogTitle>Guarda tu Regex</DialogTitle>
+                    <DialogDescription>
+                      Guarda aqui tu regex con su titulo para poder usarla en cualquier momento
+                    </DialogDescription>
+                  </DialogHeader>
+                  <div className="grid gap-4">
+                    <div className="grid gap-3">
+                      <Label htmlFor="title-1">Titulo</Label>
+                      <Input id="title-1" name="title" placeholder="Titulo de tu regex" />
+                    </div>
+                    <div className="grid gap-3">
+                      <Label htmlFor="regex-1">Regex</Label>
+                      <Input id="regex-1" name="regex" placeholder="^\d{2}-\d{2}-\d{4}$" />
+                    </div>
+                  </div>
+                  <DialogFooter>
+                    <DialogClose asChild>
+                      <Button variant="outline">Cancelar</Button>
+                    </DialogClose>
+                    <Button type="submit">Guardar</Button>
+                  </DialogFooter>
+                </DialogContent>
+              </form>
+            </Dialog>
+          </div>
+        </SidebarGroup> */}
       </SidebarContent>
+      <hr className='w-11/12 mx-auto p-2' />
+      <SidebarFooter className="mb-5">
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <div className="flex row gap-3 align-center justify-around">
+              <Link href="https://benito-tridella-dolce.vercel.app/" target='_blank'>
+                <Globe />
+              </Link>
+              <Link href={"https://www.linkedin.com/in/benito-tridella-dolce/"} target='_blank'>
+                <Linkedin />
+              </Link>
+              <Link href={"https://github.com/Benit0Trdlla"} target='_blank'>
+                <Github />
+              </Link>
+            </div>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
     </Sidebar>
   )
 }
