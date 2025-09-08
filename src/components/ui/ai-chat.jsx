2249c2f831d2a6d25add1b6c1df09c3fed515892
Benito Tridella Dolce <benitotridelladolce@gmail.com>
2025-09-08 09:15:37 -0300

    Add. AI elements SDK.

--- src/components/ui/ai-chat.jsx
+++ src/components/ui/ai-chat.jsx
@@ -1,9 +1,12 @@
 import { Markdown } from "../markdown";
+import { Conversation, ConversationContent, ConversationScrollButton } from "../ai-elements/conversation";
+import { Message, MessageContent } from "../ai-elements/message";
+import { Response } from "../ai-elements/response";
 
 const AIChat = ({ messages }) => {
     return (
         <div className='h-95 overflow-y-scroll px-6 sm:px-0'>
-            {messages.map(message => (
+            {/* {messages.map(message => (
                 <div key={message.id} className="whitespace-pre-wrap">
                     {message.role === 'user' ? 'User: ' : 'AI: '}
                     {message.parts.map((part, i) => {
@@ -17,7 +20,30 @@ const AIChat = ({ messages }) => {
                         }
                     })}
                 </div>
-            ))}
+            ))} */}
+            <Conversation>
+                <ConversationContent>
+                    {messages.map((message) => (
+                        <Message from={message.role} key={message.id}>
+                            <MessageContent>
+                                {message.parts.map((part, i) => {
+                                    switch (part.type) {
+                                        case 'text': // we don't use any reasoning or tool calls in this example
+                                            return (
+                                                <Response key={`${message.id}-${i}`}>
+                                                    {part.text}
+                                                </Response>
+                                            );
+                                        default:
+                                            return null;
+                                    }
+                                })}
+                            </MessageContent>
+                        </Message>
+                    ))}
+                </ConversationContent>
+                <ConversationScrollButton />
+            </Conversation>
         </div>
     )
 }
