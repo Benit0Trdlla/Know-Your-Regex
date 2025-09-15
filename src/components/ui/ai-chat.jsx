e0eb34aa774b3862ee013d40f648c26e4083959d
Benito Tridella Dolce <benitotridelladolce@gmail.com>
2025-09-15 09:39:51 -0300

    fix. Animation of the new 'AI elements' components.

--- src/components/ui/ai-chat.jsx
+++ src/components/ui/ai-chat.jsx
@@ -5,7 +5,7 @@ import { Response } from "../ai-elements/response";
 
 const AIChat = ({ messages }) => {
     return (
-        <div className='h-95 overflow-y-scroll px-6 sm:px-0'>
+        <div className='h-100 overflow-y-scroll px-6 sm:px-0'>
             {/* {messages.map(message => (
                 <div key={message.id} className="whitespace-pre-wrap">
                     {message.role === 'user' ? 'User: ' : 'AI: '}
@@ -21,29 +21,32 @@ const AIChat = ({ messages }) => {
                     })}
                 </div>
             ))} */}
-            <Conversation>
-                <ConversationContent>
-                    {messages.map((message) => (
-                        <Message from={message.role} key={message.id}>
-                            <MessageContent>
-                                {message.parts.map((part, i) => {
-                                    switch (part.type) {
-                                        case 'text': // we don't use any reasoning or tool calls in this example
-                                            return (
-                                                <Response key={`${message.id}-${i}`}>
-                                                    {part.text}
-                                                </Response>
-                                            );
-                                        default:
-                                            return null;
-                                    }
-                                })}
-                            </MessageContent>
-                        </Message>
-                    ))}
-                </ConversationContent>
-                <ConversationScrollButton />
-            </Conversation>
+            <div className="flex flex-col h-full">
+                <Conversation>
+                    <ConversationContent className="max-w-xl mx-auto">
+                        {messages.map((message) => (
+                            <Message from={message.role} key={message.id}>
+                                <MessageContent>
+                                    {message.parts.map((part, i) => {
+                                        switch (part.type) {
+                                            case 'text': // we don't use any reasoning or tool calls in this example
+                                                return (
+                                                    <Response key={`${message.id}-${i}`}>
+                                                        {part.text}
+                                                    </Response>
+                                                );
+                                            default:
+                                                return null;
+                                        }
+                                    })}
+                                </MessageContent>
+                            </Message>
+                        ))}
+                    </ConversationContent>
+                    <ConversationScrollButton />
+                </Conversation>
+            </div>
+
         </div>
     )
 }
